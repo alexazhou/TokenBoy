bc955c0ac5de6f5a435eb9d7bf67e6e26a480c40
AlexaZhou <AlexaZhou@163.com>
2015-11-07 16:41:45 +0800

    add tokenBoy

---
+++ config.py
@@ -0,0 +1,16 @@
+token_sources = {}
+bind_ip = '0.0.0.0'
+bind_port = 8888
+
+#token_sources['weixin'] = {
+#    'url':'https://api.weixin.qq.com/cgi-bin/token',
+#    'method':'GET',
+#    'args':{
+#        'grant_type':'client_credential',
+#        'appid':'*********************',
+#        'secret':'*********************'
+#    }
+#}
+
+
+
---
+++ tokenBoy.py
@@ -0,0 +1,63 @@
+import tornado.ioloop
+import tornado.web
+import tornado.httpclient
+import tornado.httputil
+import urllib.parse
+import time
+import json
+import config
+
+
+tokens = {}
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, tokenBoy~")
+
+class TokenHandler(tornado.web.RequestHandler):
+    def get(self):
+        name = self.get_argument('name','UnKnown')
+        ret = {}
+        if name not in tokens:
+            ret['error'] = 'not found'
+        else:
+            ret['token'] = tokens[name]
+
+        self.write( json.dumps( ret ) )
+
+application = tornado.web.Application([
+    (r"/token", TokenHandler),
+    (r"/", MainHandler),
+])
+
+
+def token_refresher():
+    print('token refresh...')
+    
+    for key in config.token_sources.keys():    
+        def handle_factory( key):
+            def handle(response):
+                #print('async httpclient response: ',response)
+                print('async httpclient response body: ',response.body)
+                ret_dict = json.loads( response.body.decode('utf8') )
+                tokens[key] = ret_dict['access_token']               
+                print('%s token:%s'%(key,tokens[key])) 
+            return handle            
+
+        #body = urllib.parse.urlencode(config.token_sources[key]['args'])
+        url = tornado.httputil.url_concat( config.token_sources[key]['url'], config.token_sources[key]['args'] )
+        async_http_client = tornado.httpclient.AsyncHTTPClient()
+        request = tornado.httpclient.HTTPRequest( url, method=config.token_sources[key]['method']  )
+        async_http_client.fetch( request, callback=handle_factory(key) )
+
+    #add next timer
+    tornado.ioloop.IOLoop.instance().call_later(3600, token_refresher)
+
+
+
+
+if __name__ == "__main__":
+    application.listen(config.bind_port,address=config.bind_ip)
+    tornado.ioloop.IOLoop.instance().call_later(1, token_refresher)
+    tornado.ioloop.IOLoop.instance().start()
+
