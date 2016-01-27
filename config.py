4a1ced83738271f3c91dca0c7ca86042d08ff7bf
AlexaZhou <AlexaZhou@163.com>
2016-01-27 13:51:44 +0800

    1，加入模版解析功能，支持在请求参数中引用其他token 2，加入刷新jssdk token的功能

--- config.py
+++ config.py
@@ -2,15 +2,27 @@ token_sources = {}
 bind_ip = '0.0.0.0'
 bind_port = 8888
 
-#token_sources['weixin'] = {
-#    'url':'https://api.weixin.qq.com/cgi-bin/token',
-#    'method':'GET',
-#    'args':{
-#        'grant_type':'client_credential',
-#        'appid':'*********************',
-#        'secret':'*********************'
-#    }
-#}
+#A example config for weixin token
+token_sources['weixin'] = {
+    'url':'https://api.weixin.qq.com/cgi-bin/token',
+    'method':'GET',
+    'args':{
+        'grant_type':'client_credential',
+        'appid':'*****************',
+        'secret':'**************************'
+    },
+}
+
+#A example config for weixin JSSDK token
+token_sources['weixin_jsticket'] = {
+    'url':'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+    'method':'GET',
+    'args':{
+        'type':'jsapi',
+        'access_token':'{{results.weixin.access_token}}' #this means the args is the access_token or weixin group
+    },
+}
+
 
 
 
--- tokenBoy.py
+++ tokenBoy.py
@@ -21,7 +21,7 @@ class TokenHandler(tornado.web.RequestHandler):
         if name not in tokens:
             ret['error'] = 'not found'
         else:
-            ret['token'] = tokens[name]
+            ret = tokens[name]
 
         self.write( json.dumps( ret ) )
 
@@ -30,38 +30,67 @@ application = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
+def args_render( s ):
 
-def token_refresher():
-    print('token refresh...')
+    if s.startswith('{{') and s.endswith("}}"):
+        render_map  = {}
+        render_map['results'] = tokens
+        s = s.strip('{}')
+        idxs = s.split('.')
+
+        target = render_map
+        for idx in idxs:
+            target = target[idx]
+
+        if type(target) != str:
+            raise Exception("render %s error"%s)
+        #print('render result:',target)
+        return target
+    else:
+        return s 
+
+def refresh_token(group):
     
-    for key in config.token_sources.keys():    
-        def handle_factory( key):
-            def handle(response):
-                print('async httpclient response: ',response)
-                print('async httpclient response body: ',response.body)
-                if response.code == 200:
-                    ret_dict = json.loads( response.body.decode('utf8') )
-                    tokens[key] = ret_dict['access_token']               
-                    print('%s token:%s'%(key,tokens[key])) 
-                else:
-                    print('request token %s error, now retry '%key)
-                    refresh(key)
-            return handle            
+    print('refresh group:',group)
 
-        def refresh( key ):
-            #body = urllib.parse.urlencode(config.token_sources[key]['args'])
-            url = tornado.httputil.url_concat( config.token_sources[key]['url'], config.token_sources[key]['args'] )
-            async_http_client = tornado.httpclient.AsyncHTTPClient()
-            request = tornado.httpclient.HTTPRequest( url, method=config.token_sources[key]['method']  )
-            async_http_client.fetch( request, callback=handle_factory(key) )
-        
-        refresh(key)
+    def handle(response):
+        #print('async httpclient response: ',response)
+        #print('async httpclient response body: ',response.body)
+        if response.code == 200:
+            ret_dict = json.loads( response.body.decode('utf8') )
+            tokens[group] = ret_dict
+            print('%s request ret:%s'%(group,tokens[group])) 
+        else:
+            print('request token %s error, will retry after 10s'%group)
+            tornado.ioloop.IOLoop.instance().call_later(10, refresh_token, group)
+
+    try:
+        #render request args
+        args = {}
+        for (k,v) in config.token_sources[group]['args'].items():
+            args[k] = args_render( v )
+
+        url = tornado.httputil.url_concat( config.token_sources[group]['url'], args )
+        async_http_client = tornado.httpclient.AsyncHTTPClient()
+        request = tornado.httpclient.HTTPRequest( url, method=config.token_sources[group]['method']  )
+        async_http_client.fetch( request, callback = handle )
+    
+    except Exception as e:
+        print('Exception:',e)
+        print('build request for %s failed, will retry after 10s'%group)
+        tornado.ioloop.IOLoop.instance().call_later(10, refresh_token, group)
 
 
+
+def refresh_all_token():
+    print('begin refresh all token...')
+    
+    for group in config.token_sources.keys():    
+        refresh_token(group)
 
 
 if __name__ == "__main__":
     application.listen(config.bind_port,address=config.bind_ip)
-    tornado.ioloop.IOLoop.instance().call_later(0, token_refresher)
-    tornado.ioloop.PeriodicCallback(token_refresher,7000*1000).start()
+    tornado.ioloop.IOLoop.instance().call_later(0, refresh_all_token)
+    tornado.ioloop.PeriodicCallback(refresh_all_token,7000*1000).start()
     tornado.ioloop.IOLoop.instance().start()
